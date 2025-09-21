ed6c3100c580de95b8968bae36f8768e4b1796dd
Ville Kärkkäinen <ville.karkkainen@nordhero.com>
2025-09-21 10:07:40 +0300

    feat: support directory bucket

--- src/main/java/concurrencyeval/Event.java
+++ src/main/java/concurrencyeval/Event.java
@@ -1,9 +1,10 @@
 package concurrencyeval;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record Event(
-        @JsonProperty("s3_bucket_name")
+        @JsonAlias({"s3_bucket_name", "s3BucketName"})
         String s3BucketName,
 
         @JsonProperty("folder")
